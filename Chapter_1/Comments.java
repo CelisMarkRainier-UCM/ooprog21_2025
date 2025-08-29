d5de0453e1d4f0b882beb99e173327924f3a9d0c
CelisMarkRainier-UCM <celismarkrainier888@gmail.com>
2025-08-29 21:19:36 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,21 @@
+/**
+ * Comments.java
+ * This code shows that there are three different types of comments in Java:
+ * Single-line comment, Multi-line comment, Documentation comment
+ *
+ *
+ */
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment.
+        // It is commonly used for short explanations.
+
+        /*
+         * This is a multi-line comment.
+         * It is often used for longer explanations,
+         * notes, or temporarily disabling blocks of code.
+         */
+
+        System.out.println("Hello! This program shows the three types of comments in Java.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Line 1: I was a ghost, I was alone (Hah)");
+        System.out.println("Line 2: I'm done hidin', now I'm shinin' like I'm born to be");
+        System.out.println("Line 3: We're goin' up, up, up, it's our moment");
+        System.out.println("Line 4: Gonna be, gonna be golden");
+    }
+}
