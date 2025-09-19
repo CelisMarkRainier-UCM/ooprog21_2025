08d515524ed169bc396f0a126bb86220fa257da9
CelisMarkRainier-UCM <celismarkrainier888@gmail.com>
2025-09-19 09:22:53 +0800

    Add files via upload

---
+++ Chapter_3/DebugThree1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DebugThree1 {
+
+   public static void main(String[] args) {
+   
+      Scanner input = new Scanner(System.in);
+      
+      System.out.print("Enter the amount of your check : ");
+      double check1 = input.nextDouble();
+      
+      System.out.print("Enter the amount of your friend's check : ");
+      double check2 = input.nextDouble();
+      
+      calcTip(check1);
+      calcTip(check2);
+   }
+   
+   public static void calcTip(double bill) {
+   
+      double rate = 0.15;
+      double tip = bill * rate;
+      System.out.println("The tip should be at least $" + tip);
+   }
+   
+}
---
+++ Chapter_3/DemoGrossPay.java
@@ -0,0 +1,15 @@
+public class DemoGrossPay {
+
+    public static void main(String[] args) {
+        calculateGross(10);
+        calculateGross(25);
+        calculateGross(37.5);
+    }
+
+    public static void calculateGross(double hoursWorked) {
+        double hourlyRate = 22.75;
+        double grossPay = hoursWorked * hourlyRate;
+
+        System.out.println(hoursWorked + " hours at $" + hourlyRate + " per hour is $" + grossPay);
+    }
+}
